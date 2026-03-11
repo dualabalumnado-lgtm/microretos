c00401f16e3c4b6f19b870d8ecfe02c76031012e
CynthiaMartel <cynthiamartelmartin@gmail.com>
2026-03-11 22:40:35 +0000

    Laravel como router principal para Vue SPA

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    // Buscamos  archivo de Vue compilado
+    $path = public_path('index.html');
+    
+    // Si existe, se lo mandamos al usuario
+    if (file_exists($path)) {
+        return file_get_contents($path);
+    }
+    
+    return "Error: No se encuentra el frontend compilado.";
+})->where('any', '.*');
