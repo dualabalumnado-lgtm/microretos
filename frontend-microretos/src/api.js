551af04e90664bdb17d3998dc0f0dd390f3654ba
CynthiaMartel <cynthiamartelmartin@gmail.com>
2026-03-11 22:24:55 +0000

    Fix variable no definida y headers api

--- frontend-microretos/src/api.js
+++ frontend-microretos/src/api.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: '/api',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
 });
 
 export default api;
