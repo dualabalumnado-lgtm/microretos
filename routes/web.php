2d0f053127394f9114cbbf64ebc8d95ce92d0abf
CynthiaMartel <cynthiamartelmartin@gmail.com>
2026-03-11 22:08:47 +0000

    Corregido htaccess para que cargue Vue (index.html)

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Le decimos a Laravel: "Cualquier cosa que escriban en la URL, 
-// mándala a la vista 'welcome' y deja que Vue.js decida qué pantalla mostrar"
-Route::get('/{any}', function () {
+Route::get('/', function () {
     return view('welcome');
-})->where('any', '.*');
+});
