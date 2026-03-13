9519bdbea02de9818fde641477da5f7f523ea355
Cynthia <cynthia@MacBook-Air-de-Cynthia.local>
2026-03-12 22:40:19 -0700

    Ajuste de CORS y producción

--- frontend-microretos/src/api.js
+++ frontend-microretos/src/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// Esta línea es la clave: 
+// Si existe la variable de producción la usa, si no, usa '/api' para local
+const apiURL = import.meta.env.VITE_API_URL || '/api';
+
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: apiURL,
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
